0c8b2f00a559ff2e155172e59f4dfe7808af776b
felipe gomez <ggfelipegc@gmail.com>
2023-08-25 13:54:36 -0500

    feat: add user type

---
+++ src/types/user.d.ts
@@ -0,0 +1,12 @@
+export type User = {
+  id: string;
+  login: string;
+  displayName: string;
+  type: string;
+  broadcasterType: string;
+  description: string;
+  profileImageUrl: string;
+  offlineImageUrl: string;
+  viewCount: number;
+  createdAt: string;
+};
