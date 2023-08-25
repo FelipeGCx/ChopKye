9add8c71779c78bcb9f4dc1d06e68412f2eb7375
felipe gomez <ggfelipegc@gmail.com>
2023-08-25 13:56:54 -0500

    feat: fetching user data

--- src/components/messageCard.vue
+++ src/components/messageCard.vue
@@ -1,15 +1,35 @@
 <script setup lang="ts">
-import { Message } from "@/types";
+import { ProductionProvider } from "@/services/requestAdapter/productionProvider";
+import { RequestService } from "@/services/requestAdapter/requestService";
+import { Message, User } from "@/types";
+import { keysToCamel } from "@/utils/keysMapping";
+import { onMounted, ref } from "vue";
 
 interface Props {
   message: Message;
 }
 
 const props = defineProps<Props>();
+const user = ref<User>();
+
+const getUser = async (url: string) => {
+  const requestProvider = new ProductionProvider();
+  const requestService = new RequestService(requestProvider);
+  const requestResponse = await requestService.getRequest(url);
+  if (requestResponse.data) {
+    user.value = keysToCamel(requestResponse.data[0]);
+  }
+};
+
+onMounted(() => {
+  const url = `users?login=${props.message.tags.username}`;
+  getUser(url);
+});
 </script>
 
 <template>
-  <div>
+  <div v-if="user">
+    <img :src="user.profileImageUrl" width="300" height="300" />
     <h1>{{ props.message.tags.displayName }}</h1>
     <p>{{ props.message.message }}</p>
   </div>
