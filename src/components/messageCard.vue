e815b799504eab4de013f6f6bbe7e6ee2402a66b
felipe gomez <ggfelipegc@gmail.com>
2023-08-25 11:48:19 -0500

    feat: add message component

---
+++ src/components/messageCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Message } from "@/types";
+
+interface Props {
+  message: Message;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div>
+    <h1>{{ props.message.tags.displayName }}</h1>
+    <p>{{ props.message.message }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
