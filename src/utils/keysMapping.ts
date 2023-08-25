e312ac2a34324f3312d0955894278d152e73cc4f
felipe gomez <ggfelipegc@gmail.com>
2023-08-25 13:53:20 -0500

    feat: implement snake_case

--- src/utils/keysMapping.ts
+++ src/utils/keysMapping.ts
@@ -1,11 +1,11 @@
-const kebabToCamel = (kebab: string): string => {
-  return kebab.replace(/-([a-z])/g, (_, char) => char.toUpperCase());
+const convertToCamel = (input: string): string => {
+  return input.replace(/[-_]([a-z])/g, (_, char) => char.toUpperCase());
 };
 
 export const keysToCamel = (element: any): any => {
   const camelKeys: { [key: string]: any } = {};
   for (const key in element) {
-    const camelKey = kebabToCamel(key);
+    const camelKey = convertToCamel(key);
     camelKeys[camelKey] = element[key];
   }
   return camelKeys;
