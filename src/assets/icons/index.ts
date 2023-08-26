f0b0349df5a94dccf2947a8feaf12f124656ae51
felipe gomez <ggfelipegc@gmail.com>
2023-08-26 13:08:49 -0500

    feat: add components icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as ModeratorIcon } from "./ModeratorIcon.vue";
+export { default as VipIcon } from "./VipIcon.vue";
+export { default as BroadcasterIcon } from "./BroadcasterIcon.vue";
+export { default as StaffIcon } from "./StaffIcon.vue";
+export { default as PrimeIcon } from "./PrimeIcon.vue";
+export { default as TurboIcon } from "./TurboIcon.vue";
+export { default as VerifiedIcon } from "./VerifiedIcon.vue";
+export { default as ArtistIcon } from "./ArtistIcon.vue";
