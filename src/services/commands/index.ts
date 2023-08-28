28bf3231eaacf747fc288b13e1cfa46fab5bf010
felipe gomez <ggfelipegc@gmail.com>
2023-08-28 15:48:15 -0500

    feat: add commands service

---
+++ src/services/commands/index.ts
@@ -0,0 +1,30 @@
+import CommandResponses from "./responses.json";
+import Commands from "./commands.json";
+export class CommandsService {
+  responses = CommandResponses;
+  commands = Commands;
+
+  getResponse(command: string): string | null {
+    const commandKey = this.getCommandKey(command);
+    let response = null;
+    if (commandKey) {
+      if (Object.keys(this.responses).includes(commandKey)) {
+        response = this.responses[commandKey].response;
+      }
+    }
+    return response;
+  }
+
+  getCommandKey(command: string): string | null {
+    let key = null;
+    for (const k in this.commands) {
+      const keyElement = this.commands[k];
+      if (keyElement.keywords.includes(command.toLocaleLowerCase())) {
+        key = keyElement.key;
+        console.log(key);
+        break;
+      }
+    }
+    return key;
+  }
+}
