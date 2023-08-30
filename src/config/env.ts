4033c7dac01d139e0064610ae5f788453e959af9
felipe gomez <ggfelipegc@gmail.com>
2023-08-30 10:45:57 -0500

    feat: add message time to display on screen

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,3 +2,5 @@ export const ID_CLIENT = import.meta.env.VITE_ID_CLIENT as string;
 export const BOT_USERNAME = import.meta.env.VITE_BOT_USERNAME as string;
 export const CHANNEL_NAME = import.meta.env.VITE_CHANNEL_NAME as string;
 export const AUTH_TOKEN = import.meta.env.VITE_AUTH_TOKEN as string;
+export const TIME_DISPLAY_MESSAGE = parseInt(import.meta.env
+  .VITE_TIME_DISPLAY_MESSAGE) as number;
